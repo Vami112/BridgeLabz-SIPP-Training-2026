888635cdab7b4961f279d060eb1fe81a0d00ca25
Vami112 <solankiriddhi68@gmail.com>
2026-06-14 14:23:10 +0530

    [VAMIKA]Added Strings level1 solutions

---
+++ core-java-practice/gcr-codebase/strings/level1/Question4.java
@@ -0,0 +1,20 @@
+class Question4 {
+
+    static void generate() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    static void handle() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        handle();
+    }
+}
