0176249c5366ae3acb56a362463c9c8efdd5a860
Vami112 <solankiriddhi68@gmail.com>
2026-06-17 12:43:15 +0530

    [VAMIKA]Added Methods recursion solutions

---
+++ core-java-practice/gcr-codebase/recursion/Question3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Question3 {
+
+    static int sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(sum(n));
+    }
+}
---
+++ core-java-practice/gcr-codebase/recursion/ScenerioBased/Scenerio1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Scenerio1 {
+
+    static boolean check(int[] arr, int i) {
+        if (i == arr.length - 1) {
+            return true;
+        }
+
+        if (arr[i] >= arr[i + 1]) {
+            return false;
+        }
+
+        return check(arr, i + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(check(arr, 0));
+    }
+}
