a428209476f17a5b9e251175e6b157f0de46c2f7
Vamika <solankiriddhi68@gmail.com>
2026-07-09 00:00:28 +0530

    [Vamika]Added merge sort and quick sort practice programs

---
+++ collections-and-dsa/gcr-codebase/efficient_sorting/CustomerOrders .java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class CustomerOrders {
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (i = 0; i < temp.length; i++)
+            arr[left + i] = temp[i];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        mergeSort(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
