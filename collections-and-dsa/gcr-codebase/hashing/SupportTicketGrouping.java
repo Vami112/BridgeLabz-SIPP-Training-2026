6ac31a313233b28a4e0c90f8e2ff80a3a1d6532d
Vamika <solankiriddhi68@gmail.com>
2026-07-20 10:51:09 +0530

    [Vamika]Added 5th program  using HashMap

---
+++ collections-and-dsa/gcr-codebase/hashing/SupportTicketGrouping.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SupportTicketGrouping {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {
+                "eat", "tea", "tan", "ate", "nat", "bat"
+        };
+
+        System.out.println(groupAnagrams(strs));
+    }
+}
