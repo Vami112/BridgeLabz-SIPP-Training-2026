74560d70f6346d308222be2730ceea29275ff688
Vamika <solankiriddhi68@gmail.com>
2026-07-23 10:04:48 +0530

    [Vamika]added binary search tree's 5th program

---
+++ collections-and-dsa/gcr-codebase/bst/KthSmallestBST.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class KthSmallestBST {
+
+    int kthSmallest(Node root, int k) {
+
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+        int count = 0;
+
+        while (current != null || !stack.isEmpty()) {
+
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            count++;
+
+            if (count == k)
+                return current.val;
+
+            current = current.right;
+        }
+
+        return -1;
+    }
+}
