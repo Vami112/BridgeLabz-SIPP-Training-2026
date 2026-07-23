56dc23601615eb4ed00102bfbd9aa1c4cd93d5a0
Vamika <solankiriddhi68@gmail.com>
2026-07-23 09:41:07 +0530

    [Vamika]added binary search tree's 3rd program

---
+++ collections-and-dsa/gcr-codebase/bst/DeleteBST.java
@@ -0,0 +1,47 @@
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class DeleteBST {
+
+    Node delete(Node root, int value) {
+
+        if (root == null)
+            return null;
+
+        if (value < root.val)
+            root.left = delete(root.left, value);
+
+        else if (value > root.val)
+            root.right = delete(root.right, value);
+
+        else {
+
+            if (root.left == null && root.right == null)
+                return null;
+
+            if (root.left == null)
+                return root.right;
+
+            if (root.right == null)
+                return root.left;
+
+            Node temp = root.right;
+
+            while (temp.left != null)
+                temp = temp.left;
+
+            root.val = temp.val;
+
+            root.right = delete(root.right, temp.val);
+        }
+
+        return root;
+    }
+}
